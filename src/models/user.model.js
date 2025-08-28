414e42c9f169bfb5d9b3969939eb7d8a8f9eef8c
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-28 23:14:18 +0530

    feat(auth): add jwt token generation methods to user model
    implement generateAccessToken and generateRefreshToken methods for user authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,4 +62,31 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_expiry,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
